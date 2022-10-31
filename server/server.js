0f88be36dae802158e781e21dd24dce956629346
leehyewon0531 <ifpffk@naver.com>
2022-10-31 22:52:10 +0900

    refactor: 전시회 데이터 DB에 저장 후 불러오기

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
-const mysql = require('./db')()
+const mysql = require('./db')
 const cors = require('cors')
 require('dotenv').config(); 
 const port = process.env.PORT || 5000
-const exhibitionRouter = require('./router/exhibition')
+const exhibitionRouter = require('./router/exhibition');
+const { dbTest } = require('./db');
 
 app.use(express.json())
 app.use(cors())
@@ -16,7 +17,9 @@ app.get('/', (req, res) => {
   res.send("hi")
 })
 
-app.use('/api/exhibition', exhibitionRouter)
+// app.use('/api/exhibition', exhibitionRouter)
 
-const connection = mysql.init()
-mysql.open(connection)
+app.use('/api/exhibition', async (req, res) => {
+  let testData = await dbTest()
+  res.send(testData)
+})
